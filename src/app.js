aa864adbc0fcbb1d1041aab2df53d8a447a3a4d2
Logan Pritchett <logan.pritchett01@gmail.com>
2024-03-24 19:48:53 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const logger = require("./config/logger");
 const articlesRouter = require("./articles/articles.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.use("/articles", articlesRouter);
 
